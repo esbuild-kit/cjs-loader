d6c54e7a14ded620ac5a519f8ee972d81f800b22
Hiroki Osame <hiroki.osame@gmail.com>
2022-05-24 20:47:57 -0400

    fix(typescript): resolve js to ts

--- tests/specs/typescript/ts.ts
+++ tests/specs/typescript/ts.ts
@@ -31,6 +31,30 @@ export default testSuite(async ({ describe }, node: NodeApis) => {
 			});
 		});
 
+		describe('full path via .js', ({ test }) => {
+			const importPath = './lib/ts-ext-ts/index.js';
+
+			test('Load - should not work', async () => {
+				const nodeProcess = await node.load(importPath);
+				expect(nodeProcess.stderr).toMatch('Cannot find module');
+			});
+
+			test('Import', async () => {
+				const nodeProcess = await node.import(importPath, { mode: 'typescript' });
+
+				if (semver.satisfies(node.version, nodeSupportsImport)) {
+					expect(nodeProcess.stderr).toMatch('Cannot find module');
+				} else {
+					expect(nodeProcess.stdout).toBe(`${output}\n{"default":1234}`);
+				}
+			});
+
+			test('Require', async () => {
+				const nodeProcess = await node.require(importPath, { typescript: true });
+				expect(nodeProcess.stdout).toBe(`${output}\n{"default":1234}`);
+			});
+		});
+
 		describe('extensionless', ({ test }) => {
 			const importPath = './lib/ts-ext-ts/index';
 
