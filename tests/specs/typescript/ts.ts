19a18604cbc75b143d08bdf1c963936e0d106577
hiroki osame <hiroki.osame@gmail.com>
2022-05-31 16:23:27 -0400

    test(typescript): verify subextensions work (#6)

--- tests/specs/typescript/ts.ts
+++ tests/specs/typescript/ts.ts
@@ -79,6 +79,31 @@ export default testSuite(async ({ describe }, node: NodeApis) => {
 			});
 		});
 
+		describe('extensionless with subextension', ({ test }) => {
+			const importPath = './lib/ts-ext-ts/index.tsx';
+			const outputSubextension = 'loaded ts-ext-ts/index.tsx.ts {"nodePrefix":true,"hasDynamicImport":true,"nameInError":true,"sourceMap":true}';
+
+			test('Load', async () => {
+				const nodeProcess = await node.load(importPath);
+				expect(nodeProcess.stdout).toBe(outputSubextension);
+			});
+
+			test('Import', async () => {
+				const nodeProcess = await node.import(importPath);
+
+				if (semver.satisfies(node.version, nodeSupportsImport)) {
+					expect(nodeProcess.stderr).toMatch('Cannot find module');
+				} else {
+					expect(nodeProcess.stdout).toBe(`${outputSubextension}\n{"default":1234}`);
+				}
+			});
+
+			test('Require', async () => {
+				const nodeProcess = await node.require(importPath);
+				expect(nodeProcess.stdout).toBe(`${outputSubextension}\n{"default":1234}`);
+			});
+		});
+
 		describe('directory', ({ test }) => {
 			const importPath = './lib/ts-ext-ts';
 
