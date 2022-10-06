966e78dade2b88f0bfd0d522cbf36b473d3d5778
Hiroki Osame <hiroki.osame@gmail.com>
2022-10-07 02:38:12 +0900

    wip

--- tests/fixtures/lib/esm-ext-mjs/index.mjs
+++ tests/fixtures/lib/esm-ext-mjs/index.mjs
@@ -37,10 +37,6 @@ test(
 	() => {
 		const { stack } = new Error();
 		const filePath = (typeof __filename === 'string') ? __filename : import.meta.url;
-		console.log({
-			stack,
-			filePath,
-		});
 		return (
 			stack.includes(filePath + ':38:')
 			|| stack.includes(filePath.toLowerCase() + ':38:')
